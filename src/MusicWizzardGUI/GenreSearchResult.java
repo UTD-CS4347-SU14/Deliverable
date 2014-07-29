483f6adec90ed80c0bdcce312246b7562327f0a3
Zack Urben <zackurben@gmail.com>
2014-07-28 23:59:23 -0500

    [Zack] Updating the GenereSearchResult view.

--- src/MusicWizzardGUI/GenreSearchResult.java
+++ src/MusicWizzardGUI/GenreSearchResult.java
@@ -1,20 +1,32 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package learn;
-
 /**
+ * The University of Texas at Dallas
+ * CS 4347.0U1 - Final Project Deliverable
  *
- * @author Kristen
+ * The Wizard's Music Shop
+ * https://github.com/UTD-CS4347-SU14/Deliverable
+ *
+ * @author Kristen Williams
+ * @author Scarlett Hunt
+ * @author Spencer Fusco
+ * @author Zack Urben
  */
+package MusicWizzardGUI;
+
+import MusicWizzard.Main;
+
 public class GenreSearchResult extends javax.swing.JFrame {
+    private Main main;
 
     /**
-     * Creates new form GenreSearchResult
+     * Create a GenreSearchResult view.
+     *
+     * @param main The reference to our view controller.
      */
-    public GenreSearchResult() {
+    public GenreSearchResult(Main main) {
         initComponents();
+
+        this.main = main;
+        this.setVisible(false);
     }
 
     /**
@@ -110,45 +122,15 @@ public class GenreSearchResult extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    /**
+     * Exit button event action.
+     *
+     * @param evt
+     */
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
         System.exit(0);
     }//GEN-LAST:event_jButton1ActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(GenreSearchResult.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(GenreSearchResult.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(GenreSearchResult.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(GenreSearchResult.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new GenreSearchResult().setVisible(true);
-            }
-        });
-    }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
