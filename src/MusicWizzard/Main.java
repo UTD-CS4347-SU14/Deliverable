ab94da006aedb5f77f82af6a8191fc1d7e8f09df
Zack Urben <zackurben@gmail.com>
2014-07-28 23:55:26 -0500

    [Zack] Updating the view handler.

--- src/MusicWizzard/Main.java
+++ src/MusicWizzard/Main.java
@@ -1,33 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * The University of Texas at Dallas
+ * CS 4347.0U1 - Final Project Deliverable
+ *
+ * The Wizard's Music Shop
+ * https://github.com/UTD-CS4347-SU14/Deliverable
+ *
+ * @author Kristen Williams
+ * @author Scarlett Hunt
+ * @author Spencer Fusco
+ * @author Zack Urben
  */
 package MusicWizzard;
 
 import MusicWizzardGUI.*;
 
-/**
- *
- * @author zack
- */
 public class Main {
-
     public StoreActivity storeActivity;
     public Inventory inventory;
+    public GenreSearchResult genreSearchResult;
 
+    /**
+     * The enumerate values for each view related to this view handler.
+     */
     public enum view {
-        StoreActivity, Inventory
+        StoreActivity, Inventory, GenreSearchResult, GenreSearch,
+        DeliveryResult, Delivery
     }
 
+    /**
+     * Create our view handler with the default settings.
+     */
     public Main() {
         this.storeActivity = new StoreActivity(this);
-        this.storeActivity.setVisible(true);
-
         this.inventory = new Inventory(this);
-        this.inventory.setVisible(false);
+        this.genreSearchResult = new GenreSearchResult(this);
     }
 
+    /**
+     * Show the given view.
+     * 
+     * @param v The view to show.
+     */
     public void showView(view v) {
         switch (v) {
             case StoreActivity:
@@ -36,9 +49,17 @@ public class Main {
             case Inventory:
                 this.inventory.setVisible(true);
                 break;
+            case GenreSearchResult:
+                this.genreSearchResult.setVisible(true);
+                break;
         }
     }
 
+    /**
+     * Hide the given view.
+     * 
+     * @param v The view to hide.
+     */
     public void hideView(view v) {
         switch (v) {
             case StoreActivity:
@@ -46,6 +67,9 @@ public class Main {
                 break;
             case Inventory:
                 this.inventory.setVisible(false);
+                break;
+            case GenreSearchResult:
+                this.genreSearchResult.setVisible(false);
                 break;
         }
     }
